17e8015c99b731e2004ef15ad033da592c4c4168
joe052 <joekaranjasenior52@gmail.com>
2022-02-19 16:33:30 +0300

    essage

--- EnumTest.java
+++ EnumTest.java
@@ -1,20 +1,35 @@
 import java.util.*;
 
 public class EnumTest {
+
+    public static String[] push(String arr[],String x){
+        int n = arr.length;
+        int i;
+
+        //create a new array of size n+1
+        String newArr[] = new String[n +1];
+
+        //insert all ements from previous array into new array
+        for(i = 0; i < n; i++){
+            newArr[i] = arr[i];
+        }
+
+        //add new element
+        newArr[n] = x;
+        return newArr;
+    }
    
     public static void main(String[] args) {
-       String[] colors = new String[100];
+       String[] colors = new String[0];
        Color c1 = Color.RED;
        System.out.println("My value is "+ c1.getValue());
-       System.out.println("My value is "+ c1);
+       System.out.println("My value is "+ c1 +"\n");
 
        for(Color color: Color.values()){
-           String nam = "blue";
-           System.out.println("\nMy value is "+ color.getValue());
-           
-           colors[0] = color.getValue();
+        System.out.println("My value is "+ color.getValue());
+        colors = push(colors, color.getValue());
+        //System.out.println("Updated array is\n"+Arrays.toString(colors));
        }
-
-       System.out.println(colors[2]);
+       System.out.println("\nUpdated array is\n"+Arrays.toString(colors));
     }
 }
